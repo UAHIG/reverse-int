003835498ca407400ccb5a953517edda518c94f8
UaHig <golosnyyigor@gmail.com>
2023-08-18 14:42:55 +0200

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let numberToString = n.toString();
+  let i = 0;
+  let result = "";
+  while (i < numberToString.length) {
+    result = `${numberToString[i]}${result}`;
+    i++;
+  }
+  const resNumber = parseInt(result);
+  return resNumber;
 }
